a13531d1cde6a5952b344ea0bbfdbd916770e41e
Tim Newman <timnewman9962@gmail.com>
2020-08-21 12:24:18 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VariableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n");
+
+            string strSample = "This is a sample string";
+            int nSample = 10;
+            char cSample = 'a';
+            bool bSample = true;
+            double fSample = 3.14;
+            decimal decSample = 3.14159m;
+
+            Console.WriteLine($"Outputting my variables...\n"
+                                + $"string: {strSample}\n"
+                                + $"int: {nSample}\n"
+                                + $"char: {cSample}\n"
+                                + $"bool: {bSample}\n"
+                                + $"double: {fSample}\n"
+                                + $"decimal: {decSample}\n"
+                                );
+        }
+    }
+}
